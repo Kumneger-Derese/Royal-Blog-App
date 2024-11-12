d9024fe80c91b1c26dabce2ed21070587cf2ccb1
Kumneger-Derese1621 <kumnegerderese3@gmail.com>
2024-11-12 12:05:27 +0300

    Cookie  problem fix2

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (userId, res) => {
 
   res.cookie('jwt2', token, {
     httpOnly: true,
-    secure: !process.env.NODE_ENV === 'development',
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
